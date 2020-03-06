81468c36cc4ff8428808c3e6a445edd7f5d8f1ce
shady831213 <shady831213@126.com>
2020-03-06 02:04:03 -0800

    format code and mask

--- proc_macros/src/lib.rs
+++ proc_macros/src/lib.rs
@@ -5,10 +5,13 @@ extern crate quote;
 extern crate syn;
 #[macro_use]
 extern crate lazy_static;
+extern crate regex;
 
 use proc_macro::TokenStream;
 use syn::{DeriveInput, DataStruct, Ident};
 use proc_macro2::Span;
+use regex::Regex;
+use std::u32;
 
 lazy_static! {
 static ref VALID_FORMAT_TYPE:Vec<&'static str> = vec![
@@ -31,7 +34,7 @@ static ref VALID_FORMAT_TYPE:Vec<&'static str> = vec![
 }
 
 
-#[proc_macro_derive(InsnCoding, attributes(code, mask, format))]
+#[proc_macro_derive(InsnCoding, attributes(code, format))]
 pub fn insn_coding(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = syn::parse(input).unwrap();
     let name = &ast.ident;
@@ -42,10 +45,11 @@ pub fn insn_coding(input: TokenStream) -> TokenStream {
 }
 
 
-fn insn_codeing_transform(ast: &DeriveInput, name:&Ident) -> Result<proc_macro2::TokenStream, syn::parse::Error> {
+fn insn_codeing_transform(ast: &DeriveInput, name: &Ident) -> Result<proc_macro2::TokenStream, syn::parse::Error> {
     if let syn::Data::Struct(data) = &ast.data {
-        let code = parse_int_attr(ast, "code")?;
-        let mask = parse_int_attr(ast, "mask")?;
+        let code_str= parse_code_attr(ast, "code")?;
+        let code = parse_code_value(&code_str);
+        let mask = parse_mask_value(&code_str);
         let format = parse_format_attr(ast)?;
         check_fields(data)?;
         Ok(quote!(
@@ -53,8 +57,8 @@ fn insn_codeing_transform(ast: &DeriveInput, name:&Ident) -> Result<proc_macro2:
             insn_format!(#name, #format);
             impl InsnCoding for #name {
                 fn ir(&self) ->  u32 {
-                    if (self.0 & self.mask() != self.code() & self.mask()) {
-                        panic!(format!("maskd ir 0x{:x}, masked code 0x{:x} are not match! ir 0x{:x},  code 0x{:x},  mask 0x{:x}", self.0 & self.mask(), self.code() & self.mask(), self.0, self.code(), self.mask()))
+                    if (self.0 & self.mask() != self.code()) {
+                        panic!(format!("ir 0x{:x} & mask 0x{:x} = 0x{:x}, expect 0x{:x}, it is not match code 0b{}!", self.0, self.mask(), self.0 & self.mask(), self.code(), #code_str))
                     }
                     self.0
                 }
@@ -72,38 +76,80 @@ fn insn_codeing_transform(ast: &DeriveInput, name:&Ident) -> Result<proc_macro2:
 }
 
 fn check_fields(data: &DataStruct) -> Result<bool, syn::parse::Error> {
-    if let syn::Fields::Unnamed(ref field) =  data.fields {
+    if let syn::Fields::Unnamed(ref field) = data.fields {
         if field.unnamed.len() != 1 {
-           return Err(syn::parse::Error::new(Span::call_site(), "expect struct \'name\' (u32)!"))
+            return Err(syn::parse::Error::new(Span::call_site(), "expect struct \'name\' (u32)!"));
         }
         if let syn::Type::Path(ref path) = field.unnamed[0].ty {
             if path.path.segments.len() != 1 || path.path.segments[0].ident != Ident::new("u32", proc_macro2::Span::call_site()) {
-                return Err(syn::parse::Error::new(Span::call_site(), "expect struct \'name\' (u32)!"))
+                return Err(syn::parse::Error::new(Span::call_site(), "expect struct \'name\' (u32)!"));
             }
-            return Ok(true)
+            return Ok(true);
         } else {
-            return Err(syn::parse::Error::new(Span::call_site(), "expect struct \'name\' (u32)!"))
+            return Err(syn::parse::Error::new(Span::call_site(), "expect struct \'name\' (u32)!"));
         }
     } else {
         Err(syn::parse::Error::new(Span::call_site(), "expect struct \'name\' (u32)!"))
     }
 }
 
-fn parse_int_attr(ast: &DeriveInput, name: &str) -> Result<u32, syn::parse::Error> {
-    if let syn::NestedMeta::Lit(syn::Lit::Int(ref lit)) = parse_attr(ast, name)? {
-        lit.base10_parse::<u32>()
+fn parse_code_attr(ast: &DeriveInput, name: &str) -> Result<String, syn::parse::Error> {
+    if let syn::NestedMeta::Lit(syn::Lit::Str(ref raw)) = parse_attr(ast, name)? {
+        parse_raw_bits(&raw.value())
     } else {
-        Err(syn::parse::Error::new(Span::call_site(), format!("\"{}\" is expected as u32", name)))
+        Err(syn::parse::Error::new(Span::call_site(), format!("\"{}\" is expected as string with \"0b\" prefix!", name)))
     }
+}
+
+fn parse_raw_bits(code: &str) -> Result<String, syn::parse::Error> {
+    lazy_static! {
+        static ref VALID_CODE: Regex = Regex::new("^0b[10?_]+").unwrap();
+        static ref VALID_BITS: Regex = Regex::new("^[10?]{1,32}").unwrap();
+        static ref BITS_REP: Regex = Regex::new("_|(?:0b)").unwrap();
+    }
+    if !VALID_CODE.is_match(code) {
+        return Err(syn::parse::Error::new(Span::call_site(), "code contains invalid char, valid format is ^0b[1|0|?|_]+!"));
+    }
+    let bits = BITS_REP.replace_all(code, "");
+    if !VALID_BITS.is_match(&bits) {
+        return Err(syn::parse::Error::new(Span::call_site(), "code defined num of bits more than 32!"));
+    }
+    if bits.len() < 32 {
+        Ok(ext_bits(&bits, 32))
+    } else {
+        Ok(bits.to_string())
+    }
+}
+
+fn ext_bits(bits: &str, cap: usize) -> String {
+    if bits.len() == cap {
+        bits.to_string()
+    } else {
+        ext_bits(&("?".to_owned() + bits), cap)
+    }
+}
+
+fn parse_code_value(bits: &str) -> u32 {
+    lazy_static! {
+        static ref QUE: Regex = Regex::new("[?]").unwrap();
+    }
+    u32::from_str_radix(& QUE.replace_all(bits, "0"), 2).unwrap()
+}
+
+fn parse_mask_value(bits: &str) -> u32 {
+    lazy_static! {
+        static ref ZERO: Regex = Regex::new("0").unwrap();
+    }
+    parse_code_value(&ZERO.replace_all(bits, "1"))
 }
 
 fn parse_format_attr(ast: &DeriveInput) -> Result<Ident, syn::parse::Error> {
     if let syn::NestedMeta::Meta(syn::Meta::Path(ref path)) = parse_attr(ast, "format")? {
         if let Some(ident) = path.get_ident() {
-            if VALID_FORMAT_TYPE.contains(&&format!("{}", ident)[..])  {
+            if VALID_FORMAT_TYPE.contains(&&format!("{}", ident)[..]) {
                 Ok(ident.clone())
             } else {
-                Err(syn::parse::Error::new(Span::call_site(), format!("invalid \"{}\" value \"{}\", valid values are {:?}", "format", ident,  *VALID_FORMAT_TYPE)))
+                Err(syn::parse::Error::new(Span::call_site(), format!("invalid \"{}\" value \"{}\", valid values are {:?}", "format", ident, *VALID_FORMAT_TYPE)))
             }
         } else {
             Err(syn::parse::Error::new(Span::call_site(), format!("\"{}\" is expected as Ident", "format")))
@@ -113,7 +159,7 @@ fn parse_format_attr(ast: &DeriveInput) -> Result<Ident, syn::parse::Error> {
     }
 }
 
-fn parse_attr(ast:&DeriveInput, name: &str) -> Result<syn::NestedMeta, syn::parse::Error> {
+fn parse_attr(ast: &DeriveInput, name: &str) -> Result<syn::NestedMeta, syn::parse::Error> {
     if let Some(attr) = ast.attrs.iter().find(|a| { a.path.segments.len() == 1 && a.path.segments[0].ident == name }) {
         if let syn::Meta::List(ref meta) = attr.parse_meta().unwrap() {
             if meta.nested.len() == 1 {
--- src/test.rs
+++ src/test.rs
@@ -3,8 +3,7 @@ use terminus_proc_macros::InsnCoding;
 use super::insn::{InsnCoding, Format};
 #[derive(InsnCoding)]
 #[format(B)]
-#[code(0b11_1111)]
-#[mask(0xdeadbeaf)]
+#[code("0b1??0_1110")]
 struct InsnCodingTestStruct(u32);
 impl InsnCodingTestStruct {
     fn new(ir:u32) -> InsnCodingTestStruct {
@@ -14,10 +13,10 @@ impl InsnCodingTestStruct {
 
 #[test]
 fn insn_coding_test() {
-    let item = InsnCodingTestStruct::new(0b101111);
-    assert_eq!(0b101111, item.op());
-    assert_eq!(0b101111, item.ir());
-    assert_eq!(0b111111, item.code());
-    let mask_bit:u32 = item.mask().bit_range(31,16);
-    assert_eq!(0xdead, mask_bit);
+    let item = InsnCodingTestStruct::new(0b1010_1110);
+    assert_eq!(0b10_1110, item.op());
+    assert_eq!(0b1010_1110, item.ir());
+    assert_eq!(0b1000_1110, item.code());
+    let mask_bit:u32 = item.mask().bit_range(15,0);
+    assert_eq!(0b1001_1111, mask_bit);
 }
