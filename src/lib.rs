0388c79f79babf47d6ae8f4b55f62b089606d9da
shady831213 <shady831213@126.com>
2020-03-09 22:50:12 -0700

    add exeception;refactory

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 extern crate terminus_macros;
 extern crate linkme;
 extern crate terminus_proc_macros;
+use linkme::*;
 
 mod insn;
 mod execption;
 mod decode;
 
-use linkme::*;
 
 #[cfg(test)]
 mod test;
