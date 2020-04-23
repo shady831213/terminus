b48316323b8ace12bbdd54002a704c14ff006529
shady831213 <shady831213@126.com>
2020-04-22 18:16:27 -0700

    fetch always in u16

--- src/processor/fetcher.rs
+++ src/processor/fetcher.rs
@@ -1,4 +1,4 @@
-use terminus_spaceport::memory::region::{U16Access, U32Access};
+use terminus_spaceport::memory::region::U16Access;
 use terminus_spaceport::memory::region;
 use crate::processor::ProcessorState;
 use crate::processor::insn::Instruction;
@@ -26,48 +26,29 @@ impl Fetcher {
 
     pub fn fetch(&self, pc: RegT, mmu: &Mmu) -> Result<Instruction, Exception> {
         let code = {
-            //expect compress, if is not support, raise illegeInst exception later
-            if pc.trailing_zeros() == 1 {
-                let pa = mmu.translate(pc, 2, MmuOpt::Fetch)?;
-                match U16Access::read(self.bus.deref(), pa) {
-                    Ok(data) => {
-                        //expect compress, if is not support, raise illegeInst exception later
-                        if data & 0x3 != 0x3 {
-                            data as u16 as InsnT
-                        } else {
-                            let pa_high = mmu.translate(pc + 2, 2, MmuOpt::Fetch)?;
-                            let data_high = match U16Access::read(self.bus.deref(), pa_high) {
-                                Ok(data_h) => {
-                                    (data_h as InsnT) << 16
-                                }
-                                Err(e) => match e {
-                                    region::Error::AccessErr(_, _) => return Err(Exception::FetchAccess(pc)),
-                                    region::Error::Misaligned(_) => return Err(Exception::FetchMisaligned(pc))
-                                }
-                            };
-                            data as u16 as InsnT | data_high
-                        }
-                    }
-                    Err(e) => match e {
-                        region::Error::AccessErr(_, _) => return Err(Exception::FetchAccess(pc)),
-                        region::Error::Misaligned(_) => return Err(Exception::FetchMisaligned(pc))
+            let pa = mmu.translate(pc, 2, MmuOpt::Fetch)?;
+            match U16Access::read(self.bus.deref(), pa) {
+                Ok(data) => {
+                    //expect compress, if is not support, raise illegeInst exception later
+                    if data & 0x3 != 0x3 {
+                        data as u16 as InsnT
+                    } else {
+                        let pa_high = mmu.translate(pc + 2, 2, MmuOpt::Fetch)?;
+                        let data_high = match U16Access::read(self.bus.deref(), pa_high) {
+                            Ok(data_h) => {
+                                (data_h as InsnT) << 16
+                            }
+                            Err(e) => match e {
+                                region::Error::AccessErr(_, _) => return Err(Exception::FetchAccess(pc)),
+                                region::Error::Misaligned(_) => return Err(Exception::FetchMisaligned(pc))
+                            }
+                        };
+                        data as u16 as InsnT | data_high
                     }
                 }
-            } else {
-                let pa = mmu.translate(pc, 4, MmuOpt::Fetch)?;
-                match U32Access::read(self.bus.deref(), pa) {
-                    Ok(data) => {
-                        //expect compress, if is not support, raise illegeInst exception later
-                        if data & 0x3 != 0x3 {
-                            data as u16 as InsnT
-                        } else {
-                            data as InsnT
-                        }
-                    }
-                    Err(e) => match e {
-                        region::Error::AccessErr(_, _) => return Err(Exception::FetchAccess(pc)),
-                        region::Error::Misaligned(_) => return Err(Exception::FetchMisaligned(pc))
-                    }
+                Err(e) => match e {
+                    region::Error::AccessErr(_, _) => return Err(Exception::FetchAccess(pc)),
+                    region::Error::Misaligned(_) => return Err(Exception::FetchMisaligned(pc))
                 }
             }
         };
