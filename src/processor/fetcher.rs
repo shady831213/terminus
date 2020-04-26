6e43aa2411c31eb9de3698f4d6542dba07655630
shady831213 <shady831213@126.com>
2020-04-25 22:55:30 -0700

    optimize perf

--- src/processor/fetcher.rs
+++ src/processor/fetcher.rs
@@ -8,101 +8,15 @@ use crate::processor::decode::*;
 use std::sync::Arc;
 use crate::devices::bus::Bus;
 use std::cell::RefCell;
-use std::mem::MaybeUninit;
 
 struct ICacheEntry {
-    accessed: bool,
     tag: u64,
     insn: Option<(InsnT, &'static Instruction)>,
 }
 
-struct ICacheBasket {
-    ptr: u8,
-    entries: [ICacheEntry; 16],
-}
-
-impl ICacheBasket {
-    fn new() -> ICacheBasket {
-        ICacheBasket {
-            ptr: 0,
-            entries: unsafe {
-                let mut arr: MaybeUninit<[ICacheEntry; 16]> = MaybeUninit::uninit();
-                for i in 0..16 {
-                    (arr.as_mut_ptr() as *mut ICacheEntry).add(i).write(ICacheEntry { accessed: false, tag: 0, insn: None });
-                }
-                arr.assume_init()
-            },
-        }
-    }
-
-    fn get_insn(&mut self, tag: u64) -> Option<(InsnT, &'static Instruction)> {
-        let mut ptr = self.ptr;
-        let tail = self.tail();
-        while ptr != tail {
-            let e = unsafe{self.entries.get_unchecked_mut(ptr as usize)};
-            if e.tag == tag {
-                if let Some(i) = e.insn {
-                    e.accessed = true;
-                    self.ptr = ptr;
-                    return Some(i);
-                }
-            }
-            e.accessed = false;
-            ptr = Self::next_ptr(ptr);
-        }
-        None
-    }
-
-    fn next_ptr(p: u8) -> u8 {
-        if p == 15 {
-            0
-        } else {
-            p + 1
-        }
-    }
-
-    fn prev_ptr(p: u8) -> u8 {
-        if p == 0 {
-            15
-        } else {
-            p - 1
-        }
-    }
-
-    fn tail(&self) -> u8 {
-        if self.ptr == 0 {
-            15
-        } else {
-            self.ptr - 1
-        }
-    }
-
-    fn set_entry(&mut self, tag: u64, ir:InsnT, insn: &'static Instruction) {
-        let mut ptr = self.tail();
-        let tail = self.ptr;
-        while ptr != tail {
-            let e = unsafe{self.entries.get_unchecked(ptr as usize)};
-            if e.insn.is_none() || !e.accessed {
-                break;
-            }
-            ptr = Self::prev_ptr(ptr);
-        }
-        let e = unsafe{self.entries.get_unchecked_mut(ptr as usize)};
-        e.accessed = true;
-        e.tag = tag;
-        e.insn = Some((ir, insn));
-        self.ptr = ptr;
-    }
-
-    fn invalid_all(&mut self) {
-        self.entries.iter_mut().for_each(|e| { e.insn = None })
-    }
-}
-
-
 struct ICache {
     size: usize,
-    baskets: Vec<ICacheBasket>,
+    baskets: Vec<ICacheEntry>,
 }
 
 impl ICache {
@@ -112,21 +26,32 @@ impl ICache {
             baskets: Vec::with_capacity(size),
         };
         for _ in 0..size {
-            cache.baskets.push(ICacheBasket::new())
+            cache.baskets.push(ICacheEntry {tag: 0, insn: None })
         };
         cache
     }
     #[cfg_attr(feature = "no-inline", inline(never))]
     fn get_insn(&mut self, addr: u64) -> Option<(InsnT, &'static Instruction)> {
-        unsafe {self.baskets.get_unchecked_mut(((addr >> 1) as usize) & (self.size - 1))}.get_insn(addr >> 1)
+        let e = unsafe {self.baskets.get_unchecked(((addr >> 1) as usize) & (self.size - 1))};
+        if let Some(insn) = e.insn {
+            if e.tag == addr >> 1 {
+                Some(insn)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
     }
     #[cfg_attr(feature = "no-inline", inline(never))]
     fn set_entry(&mut self, addr: u64, ir:InsnT, insn: &'static Instruction) {
-        unsafe {self.baskets.get_unchecked_mut(((addr >> 1) as usize) & (self.size - 1))}.set_entry(addr >> 1, ir, insn)
+        let e = unsafe {self.baskets.get_unchecked_mut(((addr >> 1) as usize) & (self.size - 1))};
+        e.tag = addr >> 1;
+        e.insn = Some((ir, insn));
     }
 
     fn invalid_all(&mut self) {
-        self.baskets.iter_mut().for_each(|b| { b.invalid_all() })
+        self.baskets.iter_mut().for_each(|b| { b.insn = None })
     }
 }
 
