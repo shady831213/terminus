e00194ecd82a08522695b057b8b1b37bf430816a
shady831213 <shady831213@126.com>
2020-05-05 21:35:36 -0700

    boot linux in 1 min

--- top_tests/decode_bench.rs
+++ top_tests/decode_bench.rs
@@ -10,7 +10,7 @@ fn decode_bench(b: &mut Bencher) {
     let code = vec![0x04813823u32, 0x0005c783u32, 0x06010413u32, 0x00093783u32];
     b.iter(|| {
         for c in code.iter() {
-            GDECODER.decode(*c).unwrap();
+            GDECODER.decode(c).unwrap();
         }
     });
 }
